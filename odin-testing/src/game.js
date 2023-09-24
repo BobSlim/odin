5dd59d8c782c042f72746e9a2c4a9db474b8e872
BobSlim <robertslim42@gmail.com>
2023-09-25 07:39:34 +1000

    adds Game

--- odin-testing/src/game.js
+++ odin-testing/src/game.js
@@ -56,3 +56,16 @@ export const Player = () => {
         attackEnemy,
         }
 }
+
+export const Game = () => {
+    const player1 = Player()
+    const player2 = Player()
+    const players = [player1, player2]
+    player1.setEnemy(player2)
+    player2.setEnemy(player1)
+    const startGame = () => {
+        for(let player of players){
+            player.placeRemainingShips()
+        }
+    }
+}
