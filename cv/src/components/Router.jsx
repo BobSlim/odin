2487b8e4ecc70d9ac5e926ba7670bd01f081085a
BobSlim <robertslim42@gmail.com>
2023-10-19 11:19:44 +1100

    moves root styling

---
+++ cv/src/components/Router.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import { MemoryCardGame } from './MemoryCardPage.jsx'
+import { App } from './App';
+import { Cv } from './Cv';
+
+export const paths = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        title: "Home"
+    },
+    {
+        path: "/careers",
+        element: <Cv />,
+        title: "Careers",
+    },
+    {
+        path: "/game",
+        element: <MemoryCardGame />,
+        title: "Game",
+    }
+])
+
+export const NavBar = () =>
+    <nav>
+        {paths.map(route => <Link to={route.path}>{route.title}</Link>)}
+    </nav>
