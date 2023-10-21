8987464f5d959b3546421cc2d579bba9859bf72e
BobSlim <robertslim42@gmail.com>
2023-10-22 10:33:37 +1100

    uses setup properly

--- src/tests/SpinnerInput.test.jsx
+++ src/tests/SpinnerInput.test.jsx
@@ -6,15 +6,20 @@ import userEvent from "@testing-library/user-event"
 describe("Spinner", () => {
     let container
     let user
+    let input
+    let incButton
+    let decButton
     beforeEach(() => {
         container = render(<SpinnerInput />).container;
         user = userEvent.setup()
+        input = screen.getByRole("spinbutton", { name: "" })
+        incButton = screen.getByRole("button", { name: "+" })
+        decButton = screen.getByRole("button", { name: "-" })
     })
     it("renders", () => {
         expect(container).toMatchSnapshot();
     })
     it("accepts input", async () => {
-        const input = screen.getByRole("spinbutton", { name: "" })
         await user.type(input, "123")
         expect(input.value).toMatch("123")
         await user.clear(input)
@@ -23,19 +28,16 @@ describe("Spinner", () => {
         expect(input.value).toMatch("0")
     })
     it("increments", async () => {
-        const button = screen.getByRole("button", {name: "+"})
-        await user.click(button)
-        expect(screen.getByRole("spinbutton", {name: ""}).value).toMatch("1")
+        await user.click(incButton)
+        expect(input.value).toMatch("1")
     })
     it("decrements", async () => {
-        const input = screen.getByRole("spinbutton", { name: "" })
-        const button = screen.getByRole("button", { name: "-" })
         await user.type(input, "2")
-        await user.click(button)
-        expect(screen.getByRole("spinbutton", { name: "" }).value).toMatch("1")
-        await user.click(button)
-        expect(screen.getByRole("spinbutton", { name: "" }).value).toMatch("0")
-        await user.click(button)
-        expect(screen.getByRole("spinbutton", { name: "" }).value).toMatch("0")
+        await user.click(decButton)
+        expect(input.value).toMatch("1")
+        await user.click(decButton)
+        expect(input.value).toMatch("0")
+        await user.click(decButton)
+        expect(input.value).toMatch("0")
     })
 })
