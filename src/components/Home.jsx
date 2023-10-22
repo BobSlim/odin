7c88e5578ebcdc11aa2e22837e4fc853d526dcf1
BobSlim <robertslim42@gmail.com>
2023-10-22 17:01:25 +1100

    fixes home styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,8 @@ import styles from "./Home.module.css"
 
 export const Home = () => {
     return (
-        <main className={styles.home}>Home!!</main>
+        <main className={styles.home}>
+            <h1>Home!!</h1>
+        </main>
     )
 }
