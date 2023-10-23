7e81ca97e3f62023be6a47af755ca423bd2e4289
BobSlim <robertslim42@gmail.com>
2023-10-24 09:55:21 +1100

    seperates tests

--- src/tests/SpinnerInput.test.jsx
+++ src/tests/SpinnerInput.test.jsx
@@ -2,6 +2,7 @@ import { SpinnerInput } from "@/components/SpinnerInput"
 import { fireEvent, render, screen } from '@testing-library/react';
 import { beforeEach } from "vitest";
 import userEvent from "@testing-library/user-event"
+import { useState } from "react";
 
 describe("Spinner", () => {
     let container
@@ -9,8 +10,14 @@ describe("Spinner", () => {
     let input
     let incButton
     let decButton
+
+    let setSpy = vi.fn()
+    let value = ""
+    let setValue = (newValue) => {
+        value = newValue
+    }
     beforeEach(() => {
-        container = render(<SpinnerInput />).container;
+        container = render(<SpinnerInput value={value} setValue={setValue} />).container;
         user = userEvent.setup()
         input = screen.getByRole("spinbutton", { name: "" })
         incButton = screen.getByRole("button", { name: "+" })
@@ -22,22 +29,27 @@ describe("Spinner", () => {
     it("accepts input", async () => {
         await user.type(input, "123")
         expect(input.value).toMatch("123")
-        await user.clear(input)
+    })
+    it("resets input to 0", async () => {
         await user.type(input, "-1")
         fireEvent.blur(input)
         expect(input.value).toMatch("0")
     })
+
     it("increments", async () => {
         await user.click(incButton)
         expect(input.value).toMatch("1")
     })
     it("decrements", async () => {
-        await user.type(input, "2")
+        input.value = "2"
         await user.click(decButton)
         expect(input.value).toMatch("1")
+    })
+    it("refuses to decrement below 0", async () => {
+        input.value = "1"
         await user.click(decButton)
-        expect(input.value).toMatch("0")
         await user.click(decButton)
         expect(input.value).toMatch("0")
     })
+
 })
